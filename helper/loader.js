84d6750dda581e764f539a9d245587224e9a0acd
Christophgru <christoph@familie-gruender.de>
2023-10-31 11:40:15 +0200

    File include writes into object

--- helper/loader.js
+++ helper/loader.js
@@ -1,21 +1,34 @@
-import * as THREE from "three";
+import {MeshBasicMaterial} from "three";
 
+function setupModel(data) {
+    return data.scene;
+}
 /** places object from path in scene*/
-export function loadObject(path, scene, loader,scalex,scaley,scalez,posx,posy,posz,rotx,roty,rotz){
-    let obj=loader.load( path, function ( gltf ) {
+export async function loadObject(path, scene, loader, scalex, scaley, scalez, posx, posy, posz, rotx, roty, rotz) {
+    const objData = await loader.loadAsync(path);
+
+    let obj=setupModel(objData);
+    //obj.material= new MeshBasicMaterial( { color: 0x505050 } );
+    obj.position.set(posx, posy, posz);
+    obj.scale.set(scalex,scaley,scalez);
+    obj.rotation.set(rotx,roty,rotz);
+    scene.add(obj);
+    return obj;
+
+    /*
+    loader.load( path, function ( gltf ) {
         console.log(gltf);
-        const object =gltf.scene;
-        object.material= new THREE.MeshBasicMaterial( { color: 0x505050 } );
-        scene.add(object);
-        object.scale.set(scalex,scaley,scalez);
-        object.rotation.set(rotx,roty,rotz);
-        object.position.set(posx,posy,posz)
-        return object;
+        obj =gltf.scene;
+        obj.material= new THREE.MeshBasicMaterial( { color: 0x505050 } );
+        scene.add(obj);
+        obj.scale.set(scalex,scaley,scalez);
+        obj.rotation.set(rotx,roty,rotz);
+        obj.position.set(posx,posy,posz);
 
     }, undefined, function ( error ) {
 
         console.error( error );
-
     } );//*/
-    return obj;
+
+
 }
