3cd58dde06a7ba2db763bc0be6e7e31b55a6ab77
Christophgru <christoph@familie-gruender.de>
2023-11-04 23:25:42 +0100

    Boatanimations and position optimized

--- main.js
+++ main.js
@@ -31,11 +31,12 @@ function init(){
             statue=r;
         });
 
-    loadObject('./public/sailingboat.glb',scene,loader,1,1,1,50,
-        1,0,0,Math.PI/3,0).then(r=>{
+    loadObject('./public/sailingboat.glb',scene,loader,1,1,1,-100,
+        1,0,0,-Math.PI/2,0).then(r=>{
             sailboat=r;
+            //sailboat.orientationY=Math.PI/2;
         });
-    loadObject('./public/boat_chris.glb',scene,loader,1,1,1,50,
+    loadObject('./public/boat_chris.glb',scene,loader,1,1,1,150,
         1,-50,0,Math.PI/3,0).then(r=>{
             cargoship=r;
         });
@@ -72,9 +73,9 @@ init();
 let renderZaehler=0;
 function render() {
     renderZaehler+=1;
-    const time = performance.now()/100000;
-    rotate(sailboat, renderZaehler,.2,Math.PI/2);
-    rotate(cargoship,renderZaehler,.5, Math.PI);
+    const time = performance.now()/10;
+    rotate(sailboat, -time*0.75,.8,Math.PI);
+    rotate(cargoship,time,.9, Math.PI);
     water.material.uniforms[ 'time' ].value += 1.0 / 60.0;
     renderStats()
     renderer.render( scene, camera );
