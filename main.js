b2dde5523c159d3ad8481bbfe4097fbbc6e8da8a
GoGoWe <gabriel@wuwer.net>
2023-11-11 23:04:19 +0100

    Refactor import statements and add collision detection

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 import * as THREE from 'three';
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
-import {loadObject} from "./helper/loader";
-import {createStats, initStats, renderStats} from "./helper/stats"
-import {rotate} from "./helper/animator";
-import {initEnvironment} from "./helper/environment";
-import {initCamera} from "./helper/camera";
-import {initControls} from "./helper/controls";
-import {OrbitControls} from "three/addons/controls/OrbitControls";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { loadObject } from "./helper/loader";
+import { createStats, initStats, renderStats } from "./helper/stats"
+import { rotate } from "./helper/animator";
+import { initEnvironment } from "./helper/environment";
+import { initCamera } from "./helper/camera";
+import { initControls } from "./helper/controls";
+import { OrbitControls } from "three/addons/controls/OrbitControls";
 import { FlyControls } from 'three/addons/controls/FlyControls.js';
 
 
@@ -15,6 +15,8 @@ const scene = new THREE.Scene();
 const loader = new GLTFLoader();
 const renderer = new THREE.WebGLRenderer();
 const clock = new THREE.Clock();
+var ray = new THREE.Raycaster()
+var timeAfterCollision = Date.now();
 
 let renderCount = 0;
 let water, camera, controls;
@@ -27,29 +29,29 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.toneMappingExposure = 0.5;
-document.body.appendChild( renderer.domElement );
+document.body.appendChild(renderer.domElement);
 
 //** Initialize the scene */
-function init(){
+function init() {
 
     loadObject('./public/statue_of_liberty.glb', scene, loader, 1, 1, 1, 0,
     0, 0, 0, -Math.PI / 2, 0).then(r => {
-        statue = r;
-    });
+            statue = r;
+        });
 
     loadObject('./public/sailingboat.glb', scene, loader, 1, 1, 1, -100,
-        1, 0, 0, -Math.PI / 2, 0).then(r => {
+    1, 0, 0, -Math.PI / 2, 0).then(r => {
             sailboat = r;
             //sailboat.orientationY=Math.PI/2;
         });
 
     loadObject('./public/boat_chris.glb', scene, loader, 1, 1, 1, 150,
-        1, -50, 0, Math.PI / 3, 0).then(r => {
+    1, -50, 0, Math.PI / 3, 0).then(r => {
             cargoship = r;
         });
 
 
-    camera = initCamera(innerWidth, innerHeight);
+    camera = initCamera(innerWidth, innerHeight);    
     water = initEnvironment(scene, renderer, camera);
     controls = initControls(camera, renderer);
 
@@ -65,7 +67,7 @@ function init(){
 
 }
 
-function initOrbitControls(camera,renderer,movement, autoRotate) {
+function initOrbitControls(camera, renderer, movement, autoRotate) {
     controls = new OrbitControls(camera, renderer.domElement);
     controls.target.set(0, 40, 0);
     controls.listenToKeyEvents(window);
@@ -84,88 +86,112 @@ function initOrbitControls(camera,renderer,movement, autoRotate) {
     return controls;
 }
 
-document.addEventListener("keydown", function(event) {
+//** Event listener for keydown events to change camera position*/
+document.addEventListener("keydown", function (event) {
     if (event.key === "1") {
-        if(controls instanceof OrbitControls) {
+        if (controls instanceof OrbitControls) {
             controls.dispose();
             controls = initControls(camera, renderer);
         }
     }
 });
 
-document.addEventListener("keydown", function(event) {
+document.addEventListener("keydown", function (event) {
     if (event.key === "2") {
-        if(controls instanceof FlyControls) {
+        if (controls instanceof FlyControls) {
             controls.dispose();
-            initOrbitControls(camera,renderer, true, true);
+            initOrbitControls(camera, renderer, true, true);
         }
-        if(controls.enabled === false){
+        if (controls.enabled === false) {
             controls.enabled = true;
             controls.autoRotate = true;
         }
     }
 });
 
-document.addEventListener("keydown", function(event) {
+document.addEventListener("keydown", function (event) {
     if (event.key === "3") {
-        if(controls instanceof FlyControls) {
+        if (controls instanceof FlyControls) {
             controls.dispose();
             controls = initOrbitControls(camera, renderer, false, false);
         }
-        if(controls.enabled === true){
+        if (controls.enabled === true) {
             controls.enabled = false;
             controls.autoRotate = false;
         }
-        camera.position.set( -10, 15, 30 );
-        controls.target.set(0,50,0);
+        camera.position.set(-10, 15, 30);
+        controls.target.set(0, 50, 0);
         controls.update();
     }
 });
 
-document.addEventListener("keydown", function(event) {
+document.addEventListener("keydown", function (event) {
     if (event.key === "4") {
-        if(controls instanceof FlyControls) {
+        if (controls instanceof FlyControls) {
             controls.dispose();
             controls = initOrbitControls(camera, renderer, false, false);
         }
-        if(controls.enabled === true){
+        if (controls.enabled === true) {
             controls.enabled = false;
             controls.autoRotate = false;
         }
-        camera.position.set( -20, 5, -170 );
+        camera.position.set(-20, 5, -170);
         controls.target.set(0, 40, 0);
         controls.update();
     }
 });
 
-document.addEventListener("keydown", function(event) {
+document.addEventListener("keydown", function (event) {
     if (event.key === "5") {
-        if(controls instanceof FlyControls) {
+        if (controls instanceof FlyControls) {
             controls.dispose();
             controls = initOrbitControls(camera, renderer, false, false);
         }
-        if(controls.enabled === true){
+        if (controls.enabled === true) {
             controls.enabled = false;
             controls.autoRotate = false;
         }
-        camera.position.set( -117, 8, -100 );
+        camera.position.set(-117, 8, -100);
         controls.target.set(0, 25, -143);
         controls.update();
     }
 });
 
-document.addEventListener("keydown", function(event) {
+document.addEventListener("keydown", function (event) {
     if (event.key === "6") {
         console.log(camera.position)
     }
 });
 
-//animate
+function collisionDetected(freeCollisionKey) {
+    controls.enabled = false;
+    controls.dispose();
+    console.log("Press " + freeCollisionKey + " to free the camera");
+
+
+    // Wait for keypress s before enabling controls again
+    document.addEventListener("keydown", function freeCollision(event) {
+        if (event.key === freeCollisionKey) {
+            timeAfterCollision = Date.now();
+            controls = initControls(camera, renderer);
+            controls.enabled = true;
+            document.removeEventListener("keydown", freeCollision);
+        }
+    });
+
+}   
+
 //** Animation Function to update controls and animations recursively */
 function animate() {
     const delta = clock.getDelta();
+    ray.setFromCamera(new THREE.Vector3(0, 0, 0), camera);
+    var collisionResults = ray.intersectObjects(scene.children, true);
+    if (collisionResults.length > 0 && collisionResults[0].distance < 10 && Date.now() - timeAfterCollision > 100) {
+        collisionDetected("s");        
+    }
+    
     controls.update(delta);
-    requestAnimationFrame( animate );
+    requestAnimationFrame(animate);
     render();
 }
 
