785b0f0be743678e8d64e11a541ffea58bec4276
Christophgru <christoph@familie-gruender.de>
2023-11-14 14:09:10 +0100

    numberpressing ausgelagert in controls.js

--- helper/controls.js
+++ helper/controls.js
@@ -1,9 +1,8 @@
 import {OrbitControls} from "three/addons/controls/OrbitControls.js";
 import { FlyControls } from 'three/addons/controls/FlyControls.js';
 
-export function initControls(camera,renderer){
-    let controls;
-    function initFlyControls(camera,renderer) {
+export function initControls(camera,renderer, controls){
+        function initFlyControls(camera,renderer) {
         controls = new FlyControls(camera, renderer.domElement);
 
         controls.movementSpeed = 50;
@@ -100,6 +99,104 @@ export function initControls(camera,renderer){
     });
 
     document.addEventListener('keyup', (event) => {resetArrowButtons();});
+
+
+    function initOrbitControls(camera, renderer, movement, autoRotate) {
+        controls = new OrbitControls(camera, renderer.domElement);
+        controls.target.set(0, 40, 0);
+        controls.listenToKeyEvents(window);
+        controls.enableDamping = false;
+        controls.dampingFactor = 0.05;
+        controls.screenSpacePanning = false;
+        controls.autoRotate = autoRotate;
+        controls.keys = {
+            LEFT: 'ArrowLeft', //left arrow
+            UP: 'ArrowUp', // up arrow
+            RIGHT: 'ArrowRight', // right arrow
+            BOTTOM: 'ArrowDown' // down arrow
+        }
+        controls.enabled = movement;
+        controls.update()
+        return controls;
+    }
+
+//** Event listener for keydown events to change camera position*/
+    document.addEventListener("keydown", function (event) {
+        switch (event.key) {
+            case "1":
+                if (controls instanceof OrbitControls) {
+                    controls.dispose();
+                    controls = initControls(camera, renderer);
+                }
+                document.getElementById("status").textContent="Flight";
+                document.getElementById("mainTitle").style.color="rgba(1,1,1,0)";
+                break;
+            case "2":
+                if(controls instanceof FlyControls) {
+                    controls.dispose();
+                    controls = initOrbitControls(camera, renderer, true, false);
+                }
+                controls.enabled = true;
+                controls.autoRotate = false;
+                document.getElementById("status").textContent="Orbit";
+                document.getElementById("mainTitle").style.color="rgba(1,1,1,0)";
+                break;
+            case "3":
+                if (controls instanceof FlyControls) {
+                    controls.dispose();
+                    initOrbitControls(camera,renderer, false, true);
+                }else {
+                    controls.enabled = false;
+                    controls.autoRotate = !controls.autoRotate; // TODO: Should this rely be toggled i would remove this else completely
+                    document.getElementById("status").textContent = controls.autoRotate ? "AutoP" : "Locked";
+                }
+                document.getElementById("mainTitle").style.color="rgba(1,1,1,1)";
+
+                camera.position.set(0, 80, 200);
+                controls.target.set(0, 40, 0);
+                break;
+            case "4":
+                if (controls instanceof FlyControls) {
+                    controls.dispose();
+                    controls = initOrbitControls(camera, renderer, false, false);
+                }
+                controls.enabled = false;
+                controls.autoRotate = false;
+                document.getElementById("status").textContent="Locked";
+                document.getElementById("mainTitle").style.color="rgba(1,1,1,1)";
+
+                camera.position.set(-20, 5, -170);
+                controls.target.set(0, 40, 0);
+                break;
+            case "5":
+                if (controls instanceof FlyControls) {
+                    controls.dispose();
+                    controls = initOrbitControls(camera, renderer, false, false);
+                }
+                controls.enabled = false;
+                controls.autoRotate = false;
+                document.getElementById("status").textContent="Locked";
+                document.getElementById("mainTitle").style.color="rgba(1,1,1,1)";
+
+                camera.position.set(-117, 8, -100);
+                controls.target.set(0, 25, -143);
+                break;
+            case "6":
+                if (controls instanceof FlyControls) {
+                    controls.dispose();
+                    controls = initOrbitControls(camera, renderer, false, false);
+                }
+                controls.enabled = false;
+                controls.autoRotate = false;
+                document.getElementById("status").textContent="Locked";
+                document.getElementById("mainTitle").style.color="rgba(1,1,1,0)";
+
+                camera.position.set(-10, 15, 30);
+                controls.target.set(0, 55, 0);
+                break;
+        }
+
+    });
 
     return initFlyControls(camera,renderer);
 }
